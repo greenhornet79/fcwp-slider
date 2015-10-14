f8dfdb2bc357c8a91a48ceb9f6efe0be20621486
Jeremy Green <jeremygreen@MacBook-Pro.local>
2015-10-13 20:38:38 -0600

    Initial commit

---
+++ fcwp-slider.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: FCWP Slider
+ * Plugin URI: http://www.meetup.com/Fort-Collins-WordPress-Meetup
+ * Description: A basic slider plugin built for the Fort Collins WordPress meetup
+ * Version: 1.0.0
+ * Author: Jeremy Green
+ * Author URI: http://www.meetup.com/Fort-Collins-WordPress-Meetup
+ * Text Domain: fcwp-slider
+ * License: GPL2
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// add stuff here
---
+++ readme.md
@@ -0,0 +1,3 @@
+# FCWP Slider
+
+Simple slider plugin built for the Fort Collins WordPress meetup plugin development workshop.
