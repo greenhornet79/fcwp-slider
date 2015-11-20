2b79bc0652321b78aaf78f6c3f28a203adc78ef3
Amber Hinds <amber@aucoeurdesign.com>
2015-11-19 18:28:37 -0700

    Add CPT
    Added slides custom post type and slider taxonomy

--- fcwp-slider.php
+++ fcwp-slider.php
@@ -4,13 +4,17 @@
  * Plugin URI: http://www.meetup.com/Fort-Collins-WordPress-Meetup
  * Description: A basic slider plugin built for the Fort Collins WordPress meetup
  * Version: 1.0.0
- * Author: Jeremy Green
+ * Author: Jeremy Green & Amber Hinds
  * Author URI: http://www.meetup.com/Fort-Collins-WordPress-Meetup
  * Text Domain: fcwp-slider
  * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Call in necessary files
+require plugin_dir_path( __FILE__ ) . 'lib/posttype.php';
 
 // add stuff here
